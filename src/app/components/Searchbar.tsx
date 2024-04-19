30200fc47212e6a6e8c825581ab3a60e2d71c204
nicholasweecw <nicholasweecw90@gmail.com>
2024-04-19 10:07:48 +1000

    Added searchbar functionality and design

--- src/app/components/Searchbar.tsx
+++ src/app/components/Searchbar.tsx
@@ -2,32 +2,60 @@
 
 import { useState, Fragment } from "react";
 import { Combobox, Transition } from "@headlessui/react";
+import { hawkerCentres } from "../constants";
 
 const Searchbar = () => {
   const [query, setQuery] = useState("");
+
+  const filteredHawkerCentres =
+    query === ""
+      ? hawkerCentres
+      : hawkerCentres.filter((hawkerCentre) =>
+          hawkerCentre
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, ""))
+        );
 
   return (
     <div className="searchbar">
       <Combobox>
         <div className="relative w-full">
+          {/* Input field for searching */}
           <Combobox.Input
             className="search-input"
             placeholder="Search for hawker centres..."
             displayValue={(hawkerCentre: string) => hawkerCentre}
-            onChange={(e) => setQuery(e.target.value)}
+            onChange={(e) => setQuery(e.target.value)} // Update the search query
           />
+
+          {/* Transition for displaying the options */}
           <Transition
-            as={Fragment}
+            as={Fragment} // group multiple elements without introducing an additional DOM node i.e., <></>
             leave="transition ease-in duration-100"
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
-            afterLeave={() => setQuery("")}
+            afterLeave={() => setQuery("")} // Reset the search query after the transition completes
           >
-            <Combobox.Options className="search-options">
-              <Combobox.Option value="Nasi Lemak" />
-              <Combobox.Option value="Char Kway Teow" />
-              <Combobox.Option value="Hainanese Chicken Rice" />
-              <Combobox.Option value="Laksa" />
+            <Combobox.Options>
+              {filteredHawkerCentres.length === 0 && query !== "" ? (
+                <Combobox.Option value={query} className="search-option">
+                  No results found
+                </Combobox.Option>
+              ) : (
+                filteredHawkerCentres.map((hawkerCentre) => (
+                  <Combobox.Option
+                    key={hawkerCentre}
+                    className={({ active }) =>
+                      `relative search-option
+                  ${active ? "bg-blue-600 text-white" : "text-gray-900"}`
+                    }
+                    value={hawkerCentre}
+                  >
+                    {hawkerCentre}
+                  </Combobox.Option>
+                ))
+              )}
             </Combobox.Options>
           </Transition>
         </div>
---
+++ src/app/constants/index.ts
@@ -0,0 +1,5 @@
+export const hawkerCentres = [
+  "Ang Mo Kio Ave 10 Blk 453A (Chong Boon Market and Food Centre)",
+  "Ang Mo Kio Ave 1 Blk 226D (Kebun Baru Market and Food Centre)",
+  "Ang Mo Kio Street 22 Blk 226H (Kebun Baru Food Centre)",
+];
