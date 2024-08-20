f2fb53d69ccba7d5fec608cac7928b05c3c12817
nicholasweecw <nicholasweecw90@gmail.com>
2024-08-20 16:27:10 +0800

    Brought hawker deets into Hawker Page

--- src/app/hawkerCentres/[...hawker]/page.tsx
+++ src/app/hawkerCentres/[...hawker]/page.tsx
@@ -1,6 +1,18 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import React from "react";
+import json from "../../constants/csvjson.json";
 
 const HawkerPage = () => {
+  const pathname = usePathname();
+
+  const hawkerName = decodeURIComponent(pathname.slice(15));
+
+  const hawker = json.filter((hawker) => hawker.Name === hawkerName)[0];
+
+  console.log(hawker.ADDRESSSTREETNAME);
+
   return <div>HawkerPage</div>;
 };
 
