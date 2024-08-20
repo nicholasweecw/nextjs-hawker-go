663b329261288c4ce6a5c5eee0c8ace2deaf57a7
nicholasweecw <nicholasweecw90@gmail.com>
2024-08-20 09:03:40 +0800

    Set up routing from HawkerCard to Hawker Page

--- src/app/components/HawkerCard.tsx
+++ src/app/components/HawkerCard.tsx
@@ -3,42 +3,47 @@
 import { useState } from "react";
 import { HawkerCentreProps } from "../../../types";
 import Image from "next/image";
+import Link from "next/link";
 
 interface HawkerCardProps {
   hawkerCentre: HawkerCentreProps;
 }
 
+const Routes = [{ hawkerPage: "/hawkerCentres/[...hawker]" }];
+
 const HawkerCard = ({ hawkerCentre }: HawkerCardProps) => {
   return (
-    <div className="hawker-card group">
-      <div className="hawker-card__content">
-        <div className="mr-3 my-auto h-50 object-contain">
-          <Image
-            src={hawkerCentre.PHOTOURL.toString()}
-            width={150}
-            height={100}
-            alt="Hawker Centre"
-            objectFit="cover"
-            priority
-            className="object-contain"
-          />
-        </div>
+    <Link href={`/hawkerCentres/${hawkerCentre.Name}`}>
+      <div className="hawker-card group">
+        <div className="hawker-card__content">
+          <div className="mr-3 my-auto h-50 object-contain">
+            <Image
+              src={hawkerCentre.PHOTOURL.toString()}
+              width={150}
+              height={100}
+              alt="Hawker Centre"
+              objectFit="cover"
+              priority
+              className="object-contain"
+            />
+          </div>
 
-        <div className="hawker-card__content-info">
-          <h2 className="hawker-card__content-title">
-            {hawkerCentre.Name}
-            <br />
-          </h2>
-          <h3 className="hawker-card_content-location">
-            {"Location:"} {hawkerCentre.ADDRESSSTREETNAME}
-          </h3>
-          <h3 className="hawker-card_content-closure">
-            {"Hours: "}
-            <span className="text-red-500 font-semibold">{"Closed"}</span>
-          </h3>
+          <div className="hawker-card__content-info">
+            <h2 className="hawker-card__content-title">
+              {hawkerCentre.Name}
+              <br />
+            </h2>
+            <h3 className="hawker-card_content-location">
+              {"Location:"} {hawkerCentre.ADDRESSSTREETNAME}
+            </h3>
+            <h3 className="hawker-card_content-closure">
+              {"Hours: "}
+              <span className="text-red-500 font-semibold">{"Closed"}</span>
+            </h3>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
---
+++ src/app/hawkerCentres/[...hawker]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HawkerPage = () => {
+  return <div>HawkerPage</div>;
+};
+
+export default HawkerPage;
