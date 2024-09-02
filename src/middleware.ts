4ba927465516ed4645dba695ff2401f09ef2161b
nicholasweecw <nicholasweecw90@gmail.com>
2024-09-02 15:00:08 +0800

    Temporarily commented out middleware.tx

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 // Without a defined matcher, this one line will be applied to all routes
-export { default } from "next-auth/middleware";
+// export { default } from "next-auth/middleware";
 
 // Applies next-auth only to matching routes
-export const config = {
-  // matcher: "/favourites",
-};
+// export const config = {
+//   matcher: "/favourites",
+// };
